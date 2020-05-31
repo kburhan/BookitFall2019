9874ebfb231493259997ac227a2763dc12a60d40
kburhan <kburhan07@gmail.com>
2020-05-31 16:39:08 -0400

    sunday1

---
+++ src/test/java/com/bookit/step_definitions/LoginStepDefinitions.java
@@ -0,0 +1,25 @@
+package com.bookit.step_definitions;
+import com.bookit.pages.LoginPage;
+import com.bookit.utilities.Driver;
+import com.bookit.utilities.Environment;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class LoginStepDefinitions {
+    LoginPage loginPage = new LoginPage();
+
+    @Then("user is on the landing page")
+    public void user_is_on_the_landing_page() {
+        Driver.getDriver().get(Environment.URL);
+    }
+
+    @When("user logs in with {string} email and {string} password")
+    public void user_logs_in_with_email_and_password(String string, String string2) {
+        loginPage.login(string, string2);
+    }
+
+    @When("user navigates to personal page")
+    public void user_navigates_to_personal_page() {
+        loginPage.goToSelfPage();
+    }
+}
